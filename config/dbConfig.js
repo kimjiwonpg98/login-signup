e8705c30c7bcb232ee44e01df7d2e3c13d4de739
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-16 16:22:21 +0900

    Implement: .env파일 추가

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const dbConfig = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
-  user: "root",
-  password: "dhakdlrjf11",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: "login",
 });
 
